127c35a0d9241aede8cad4bdd5a60b3a272545d9
Enrico Oliva <enrico.oliva@uniurb.it>
2026-01-30 13:57:19 +0100

    feat: aggiornamento testing

--- unicontract-backend/database/migrations/2020_05_25_152418_op_risumane.php
+++ unicontract-backend/database/migrations/2020_05_25_152418_op_risumane.php
@@ -40,7 +40,7 @@ class OpRisumane extends Migration
             $mp = new MappingRuolo();
             $mp->unitaorganizzativa_uo = $office;
             // Only try to read from Oracle in non-testing environments
-            if ($this->oracleConnected() || !app()->environment('testing')) {
+            if (app()->environment('testing')) {
                 $uo = $mp->unitaorganizzativa()->get()->first();
                 if ($uo) {
                     $mp->descrizione_uo = $uo->descr;
